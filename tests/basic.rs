2efdda37cdc2ac386d12f1f9e8bbfc72f221c315
Yushi OMOTE <yushiomote@gmail.com>
2019-11-16 10:42:05 +0900

    Add more unittests

--- tests/basic.rs
+++ tests/basic.rs
@@ -3,57 +3,136 @@ use redis_ac::Commands;
 
 mod helper;
 
-use helper::{run, setup};
+use crate::helper::*;
 
 #[test]
 fn setget() {
-    let (_s, c) = setup();
-
-    let f = c
-        .get_async_connection()
-        .and_then(|con| {
-            con.set("key", "value")
+    test(|c| {
+        c.get_async_connection().and_then(|con| {
+            con.set("key", b"value")
                 .and_then(|(con, s): (_, String)| {
                     assert_eq!(s, "OK");
                     con.get("key")
                 })
-                .map(|(_, s): (_, String)| {
-                    assert_eq!(s, "value");
+                .map(|(_, s): (_, Vec<u8>)| {
+                    assert_eq!(s, b"value");
                 })
         })
-        .map_err(|e| panic!("{}", e));
-
-    run(f).unwrap();
+    });
 }
 
 #[test]
 fn scan() {
-    let (_s, c) = setup();
+    test(|c| {
+        let exp = write_values("key");
 
-    let f = c
-        .get_shared_async_connection()
-        .and_then(|con| {
-            futures::future::join_all((0..1000).map(move |i| {
-                con.clone()
-                    .set(format!("key:{:06}", i), format!("value{}", i))
-            }))
-        })
-        .and_then(|res: Vec<(_, String)>| {
-            let con = res[0].0.clone();
-            con.scan_match("key*")
-                .map(|(_, v): (_, String)| v)
-                .collect()
-        })
-        .map(|mut res: Vec<_>| {
-            res.sort();
-            assert_eq!(
-                res,
-                (0..1000)
-                    .map(|i| format!("key:{:06}", i))
-                    .collect::<Vec<_>>()
-            )
-        })
-        .map_err(|e| panic!("{}", e));
+        c.get_async_connection()
+            .and_then(|con| con.scan().map(|(_, v)| v).collect())
+            .map(|mut res: Vec<String>| {
+                res.sort();
+                assert_eq!(res, keys(exp))
+            })
+    })
+}
 
-    run(f).unwrap();
+#[test]
+fn scan_match() {
+    test(|c| {
+        let exp = write_values("key");
+        let _ = write_values("garbage");
+
+        c.get_async_connection()
+            .and_then(|con| con.scan_match("key:*").map(|(_, v)| v).collect())
+            .map(|mut res: Vec<String>| {
+                res.sort();
+                assert_eq!(res, keys(exp))
+            })
+    })
+}
+
+#[test]
+fn hscan() {
+    test(|c| {
+        let exp = write_hash_values("hash", "key");
+
+        c.get_async_connection()
+            .and_then(|con| con.hscan("hash").map(|(_, v)| v).collect())
+            .map(move |mut res: Vec<(String, String)>| {
+                res.sort();
+                assert_eq!(res, both(exp))
+            })
+    })
+}
+
+#[test]
+fn hscan_match() {
+    test(|c| {
+        let exp = write_hash_values("hash", "key");
+        let _ = write_hash_values("hash", "garbage");
+
+        c.get_async_connection()
+            .and_then(|con| con.hscan_match("hash", "key:*").map(|(_, v)| v).collect())
+            .map(move |mut res: Vec<(String, String)>| {
+                res.sort();
+                assert_eq!(res, both(exp))
+            })
+    })
+}
+
+#[test]
+fn sscan() {
+    test(|c| {
+        let exp = write_set_values("set", "key");
+
+        c.get_async_connection()
+            .and_then(|con| con.sscan("set").map(|(_, v)| v).collect())
+            .map(move |mut res: Vec<String>| {
+                res.sort();
+                assert_eq!(res, keys(exp));
+            })
+    })
+}
+
+#[test]
+fn sscan_match() {
+    test(|c| {
+        let exp = write_set_values("set", "key");
+        let _ = write_set_values("set", "garbage");
+
+        c.get_async_connection()
+            .and_then(|con| con.sscan_match("set", "key:*").map(|(_, v)| v).collect())
+            .map(move |mut res: Vec<String>| {
+                res.sort();
+                assert_eq!(res, keys(exp));
+            })
+    })
+}
+
+#[test]
+fn zscan() {
+    test(|c| {
+        let exp = write_zset_values("zset", "key");
+
+        c.get_async_connection()
+            .and_then(|con| con.zscan("zset").map(|(_, v)| v).collect())
+            .map(move |mut res: Vec<(String, String)>| {
+                res.sort();
+                assert_eq!(res, both(exp));
+            })
+    })
+}
+
+#[test]
+fn zscan_match() {
+    test(|c| {
+        let exp = write_zset_values("zset", "key");
+        let _ = write_zset_values("zset", "garbage");
+
+        c.get_async_connection()
+            .and_then(|con| con.zscan_match("zset", "key:*").map(|(_, v)| v).collect())
+            .map(move |mut res: Vec<(String, String)>| {
+                res.sort();
+                assert_eq!(res, both(exp));
+            })
+    })
 }
--- tests/helper.rs
+++ tests/helper.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
 
+use futures::prelude::*;
 use redis::Client;
+use redis_ac::Commands;
 use std::process::{Child, Command, Stdio};
 
-pub use tokio::runtime::current_thread::block_on_all as run;
+pub use tokio::runtime::current_thread::block_on_all;
 
 pub struct Server {
     p: Option<Child>,
@@ -11,13 +13,6 @@ pub struct Server {
 
 fn sleep() {
     std::thread::sleep(std::time::Duration::from_secs(1));
-}
-
-fn flush(cli: &redis::Client) {
-    let mut con = cli.get_connection().unwrap();
-    let _con: () = redis::cmd("flushall")
-        .query(&mut con)
-        .expect("Couldn't flush");
 }
 
 impl Server {
@@ -48,19 +43,152 @@ impl Drop for Server {
     }
 }
 
-pub fn setup() -> (Server, Client) {
-    let disable = std::env::var("NO_REDIS").is_ok();
-    let port = std::env::var("PORT").unwrap_or("6379".into());
+fn port() -> String {
+    std::env::var("PORT").unwrap_or("6379".into())
+}
 
+pub fn run_server() -> Server {
+    let disable = std::env::var("NO_REDIS").is_ok();
+    Server::new(&port(), disable)
+}
+
+pub fn run_client() -> Client {
+    redis::Client::open(format!("redis://127.0.0.1:{}", port()).as_ref()).unwrap()
+}
+
+pub fn setup() -> (Server, Client) {
     // Run redis server
-    let server = Server::new(&port, disable);
+    let server = run_server();
 
     sleep();
 
-    // Run client
-    let client = redis::Client::open(format!("redis://127.0.0.1:{}", port).as_ref()).unwrap();
+    // Flush data
+    flush();
 
-    flush(&client);
+    // Run client
+    let client = run_client();
 
     (server, client)
+}
+
+pub fn run<F>(f: F)
+where
+    F: IntoFuture + Send + 'static,
+    F::Error: std::fmt::Debug,
+{
+    block_on_all(f.into_future()).unwrap();
+}
+
+/// Run with a new client and a new server with clean state.
+pub fn test<F, R>(f: F)
+where
+    F: FnOnce(Client) -> R,
+    R: IntoFuture + Send + 'static,
+    R::Error: std::fmt::Debug,
+{
+    let (_s, c) = setup();
+    run(f(c));
+}
+
+/// Run with a new client.
+pub fn with_cli<F, R>(f: F)
+where
+    F: FnOnce(Client) -> R,
+    R: IntoFuture + Send + 'static,
+    R::Error: std::fmt::Debug,
+{
+    let c = run_client();
+    run(f(c));
+}
+
+pub fn flush() {
+    with_cli(|c| {
+        let mut con = c.get_connection().unwrap();
+        let _: () = redis::cmd("flushall").query(&mut con).unwrap();
+        Ok::<_, ()>(())
+    })
+}
+
+fn write_kv<K, V, F>(data: Vec<(K, V)>, f: F) -> Vec<(K, V)>
+where
+    K: redis::ToRedisArgs + Send + Clone + 'static,
+    V: redis::ToRedisArgs + Send + Clone + 'static,
+    F: Fn(
+            redis::aio::SharedConnection,
+            K,
+            V,
+        ) -> redis::RedisFuture<(redis::aio::SharedConnection, ())>
+        + Send
+        + 'static,
+{
+    let data2 = data.clone();
+
+    with_cli(move |c| {
+        c.get_shared_async_connection()
+            .and_then(move |con| {
+                futures::future::join_all(
+                    data.into_iter()
+                        .map(move |(key, value)| f(con.clone(), key, value)),
+                )
+            })
+            .map(|_: Vec<(_, _)>| ())
+    });
+
+    data2
+}
+
+pub fn write_values(prefix: &str) -> Vec<(String, String)> {
+    let data: Vec<_> = (0..10000)
+        .map(|i| (format!("{}:{:06}", prefix, i), format!("value{}", i)))
+        .collect();
+
+    write_kv(data, |c, k, v| c.set(k, v))
+}
+
+pub fn write_hash_values(set: &str, prefix: &str) -> Vec<(String, String)> {
+    let data: Vec<_> = (0..10000)
+        .map(|i| (format!("{}:{:06}", prefix, i), format!("value{}", i)))
+        .collect();
+
+    let set = set.to_string();
+    write_kv(data, move |c, k, v| c.hset(set.clone(), k, v))
+}
+
+pub fn write_set_values(set: &str, prefix: &str) -> Vec<(String, String)> {
+    let data: Vec<_> = (0..10000)
+        .map(|i| (format!("{}:{:06}", prefix, i), "unused".into()))
+        .collect();
+
+    let set = set.to_string();
+    write_kv(data, move |c, k, _| c.sadd(set.clone(), k))
+}
+
+pub fn write_zset_values(set: &str, prefix: &str) -> Vec<(String, String)> {
+    let data: Vec<_> = (0..10000)
+        .map(|i| (format!("{}:{:06}", prefix, i), format!("{}", i)))
+        .collect();
+
+    let set = set.to_string();
+    write_kv(data, move |c, v, s| c.zadd(set.clone(), v, s))
+}
+
+pub fn both<T, K, V>(data: T) -> Vec<(K, V)>
+where
+    T: IntoIterator<Item = (K, V)>,
+{
+    data.into_iter().collect()
+}
+
+pub fn keys<T, K, V>(data: T) -> Vec<K>
+where
+    T: IntoIterator<Item = (K, V)>,
+{
+    data.into_iter().map(|(k, _)| k).collect()
+}
+
+pub fn values<T, K, V>(data: T) -> Vec<V>
+where
+    T: IntoIterator<Item = (K, V)>,
+{
+    data.into_iter().map(|(_, v)| v).collect()
 }
